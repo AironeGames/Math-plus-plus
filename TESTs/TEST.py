8921024c5ee4482e204d891a598eb753cdd46209
AironeGames <renat.aslanyan@gmail.com>
2025-01-28 10:59:48 +0300

    1.0.0
    GG

---
+++ TESTs/TEST.py
@@ -0,0 +1,4 @@
+import mpp
+
+print(mpp.plus(3,9))
+print(mpp.r_int(1,100))
---
+++ TESTs/mpp.py
@@ -0,0 +1,18 @@
+from random import randint
+
+def plus (a, b):
+	return a + b
+
+def minus (a, b):
+	return a - b
+	
+def umnogh (a, b):
+	return a * b
+	
+def razdelit (a, b):
+	return a / b
+
+def r_int(a, b):
+	result = randint(a, b)
+	return result
+	
